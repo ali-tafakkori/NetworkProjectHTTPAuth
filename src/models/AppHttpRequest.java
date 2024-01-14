5a4374bf60520ed30e22b9b7666cd070a3343a87
Ali Tafakkori <alitafakkori82@gmail.com>
2024-01-14 11:48:41 +0330

    Update AppHttpRequest.java

--- src/models/AppHttpRequest.java
+++ src/models/AppHttpRequest.java
@@ -3,4 +3,14 @@ package models;
 public class AppHttpRequest {
     public AppHttpMethod method;
     public String path;
+
+    public static AppHttpRequest parse(String data) {
+        AppHttpRequest request = new AppHttpRequest();
+        String[] split = data.split(" ");
+
+        request.method = AppHttpMethod.valueOf(split[0].trim());
+        request.path = split[1].trim();
+
+        return request;
+    }
 }
