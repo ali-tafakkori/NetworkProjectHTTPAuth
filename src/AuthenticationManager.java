fade2428d6403981d099d130f2f4591df2b238b0
Ali Tafakkori <alitafakkori82@gmail.com>
2024-01-14 18:28:31 +0330

    Update AuthenticationManager.java

--- src/AuthenticationManager.java
+++ src/AuthenticationManager.java
@@ -1,2 +1,26 @@
+import models.AppUser;
+
 public class AuthenticationManager {
+    private static AppUser[] appUsers = {
+            new AppUser("ali", "1234", "علی تفکری خسروشاهی"),
+            new AppUser("pirahesh", "3574159", "استاد"),
+    };
+
+    private static AuthenticationManager manager;
+
+    public static AuthenticationManager getInstance() {
+        if (manager == null) {
+            manager = new AuthenticationManager();
+        }
+        return manager;
+    }
+
+    public AppUser authenticate(String username, String password) {
+        for (AppUser user : appUsers) {
+            if (user.username.equals(username) && user.password.equals(password)){
+                return user;
+            }
+        }
+        return null;
+    }
 }
