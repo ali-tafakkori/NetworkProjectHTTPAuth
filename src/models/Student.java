6763d13508976344b25eb50bf425abe7d91f27f6
Ali Tafakkori <alitafakkori82@gmail.com>
2024-01-14 20:21:13 +0330

    Update Student.java

--- src/models/Student.java
+++ src/models/Student.java
@@ -20,8 +20,22 @@ public class Student {
         return new Student(split[0], split[1], split[2], split[3], split[4], split[5], split[6]);
     }
 
-    @Override
-    public String toString() {
-        return "Code: " + code + "\nFirst name: " + firstName + "\nLast name: " + lastName + "\nGrade: " + grade + "\nField: " + field;
+    public String toHtml() {
+        return "<table>\n" +
+                "  <tr>\n" +
+                "    <th>Code</th>\n" +
+                "    <th>First name</th>\n" +
+                "    <th>Last name</th>\n" +
+                "    <th>Grade</th>\n" +
+                "    <th>Field</th>\n" +
+                "  </tr>\n" +
+                "  <tr>\n" +
+                "    <td>" + code + "</td>\n" +
+                "    <td>" + firstName + "</td>\n" +
+                "    <td>" + lastName + "</td>\n" +
+                "    <td>" + grade + "</td>\n" +
+                "    <td>" + field + "</td>\n" +
+                "  </tr>\n" +
+                "</table>";
     }
 }
