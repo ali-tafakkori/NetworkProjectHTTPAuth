73e478e8e7a0a6339a15eaca0f02da2537f21605
Ali Tafakkori <alitafakkori82@gmail.com>
2024-01-14 18:28:28 +0330

    Update AppUser.java

--- src/models/AppUser.java
+++ src/models/AppUser.java
@@ -1,10 +1,11 @@
 package models;
 
 public class AppUser {
-    public final String username, password;
+    public final String username, password,name;
 
-    public AppUser(String username, String password) {
+    public AppUser(String username, String password, String name) {
         this.username = username;
         this.password = password;
+        this.name = name;
     }
 }
