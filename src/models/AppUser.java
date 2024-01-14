8553948312147a991c09daceff2a4a4e7444f516
Ali Tafakkori <alitafakkori82@gmail.com>
2024-01-14 18:21:35 +0330

    Create AppUser.java

---
+++ src/models/AppUser.java
@@ -0,0 +1,10 @@
+package models;
+
+public class AppUser {
+    public final String username, password;
+
+    public AppUser(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
